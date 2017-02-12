eaa000098c7142c4476140333fa88fc56b0a190c
Kenny Iraheta <kennyiraheta@Kennys-MacBook-Pro.local>
2017-02-12 00:37:24 -0800

    Update dashboard UI

--- src/main/resources/static/scripts/userdata.controller.js
+++ src/main/resources/static/scripts/userdata.controller.js
@@ -5,25 +5,24 @@ app.controller('UserData', function($scope, $http, $mdDialog) {
 		activity: 'Relaxation'
 	};
 
-    $scope.isRelax = function(ev){
+    $scope.isRelax = function(){
         $scope.user.activity = "Relaxation";
         if(!angular.isNumber($scope.user.age) || $scope.user.age < 1 || $scope.user.age > 999){
                 $mdDialog.show(
                   $mdDialog.alert()
                     .parent(angular.element(document.querySelector('#popupContainer')))
                     .clickOutsideToClose(true)
-                    .title('Uh Oh Something went wrong')
+                    .title('Uh Oh. Something went wrong')
                     .textContent('Probably your age...fix it!')
                     .ariaLabel('dialog alert')
                     .ok('Got it!')
-                    .targetEvent(ev)
                 );
         }else{
             $scope.user.age = Math.max(1, Math.floor($scope.user.age));
             $scope.sendJSON();
         }
     }
-    $scope.isExercise = function(ev){
+    $scope.isExercise = function(){
         $scope.user.activity = "Exercise";
 
         if(!angular.isNumber($scope.user.age) || $scope.user.age < 1 || $scope.user.age > 999){
@@ -31,11 +30,10 @@ app.controller('UserData', function($scope, $http, $mdDialog) {
                   $mdDialog.alert()
                     .parent(angular.element(document.querySelector('#popupContainer')))
                     .clickOutsideToClose(true)
-                    .title('Uh Oh Something went wrong')
+                    .title('Uh Oh. Something went wrong')
                     .textContent('Probably your age...fix it!')
                     .ariaLabel('dialog alert')
                     .ok('Got it!')
-                    .targetEvent(ev)
                 );
         }else{
             $scope.user.age = Math.max(1, Math.floor($scope.user.age));
@@ -49,9 +47,6 @@ app.controller('UserData', function($scope, $http, $mdDialog) {
     }
 
     $scope.sendJSON = function(){
-//    		$http.post({ 'User age':$scope.user.age, 'activity': $scope.user.activity });
-    		// Writing it to the server
-    		//
     		var dataObj = {
     				age : $scope.user.age,
     				activity : $scope.user.activity,
@@ -60,9 +55,6 @@ app.controller('UserData', function($scope, $http, $mdDialog) {
             		.then(function(data, status, headers, config) {
             			$scope.message = data;
             		});
-//            res.error(function(data, status, headers, config) {
-//            			alert( "failure message: " + JSON.stringify({data: data}));
-//            		});
             		// Making the fields empty
             		//
             		$scope.age='';
