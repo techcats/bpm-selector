98c59bf8fa1371bfd595701cfe239c18f2115810
Kenny Iraheta <kennyiraheta@Kennys-MacBook-Pro.local>
2017-02-11 18:58:44 -0800

    edit controller

--- src/main/resources/static/scripts/userdata.controller.js
+++ src/main/resources/static/scripts/userdata.controller.js
@@ -15,20 +15,20 @@ app.controller('UserData', function($scope, $http) {
     }
 
     $scope.sendJSON = function(){
-    		$scope.user.push({ 'User age':$scope.user.age, 'activity': $scope.user.activity });
+//    		$http.post({ 'User age':$scope.user.age, 'activity': $scope.user.activity });
     		// Writing it to the server
     		//
     		var dataObj = {
     				age : $scope.user.age,
     				activity : $scope.user.activity,
     		};
-            var res = $http.post('/user_data_json', dataObj);
-            		res.success(function(data, status, headers, config) {
+            var res = $http.post('/user_data_jso', dataObj);
+            		res.then(function(data, status, headers, config) {
             			$scope.message = data;
             		});
-            res.error(function(data, status, headers, config) {
-            			alert( "failure message: " + JSON.stringify({data: data}));
-            		});
+//            res.error(function(data, status, headers, config) {
+//            			alert( "failure message: " + JSON.stringify({data: data}));
+//            		});
             		// Making the fields empty
             		//
             		$scope.age='';
