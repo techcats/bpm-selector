916272103678b7d58dc199273e7b60b03d39b9cf
Kenny Iraheta <kennyiraheta@Kennys-MacBook-Pro.local>
2017-02-11 18:10:59 -0800

    add post in controller

--- src/main/resources/static/scripts/userdata.controller.js
+++ src/main/resources/static/scripts/userdata.controller.js
@@ -7,10 +7,31 @@ app.controller('UserData', function($scope, $http) {
 
     $scope.isRelax = function(){
         $scope.user.activity = "Relaxation";
+        $scope.sendJSON();
     }
     $scope.isExercise = function(){
         $scope.user.activity = "Exercise";
+        $scope.sendJSON();
     }
 
-	
+    $scope.sendJSON = function(){
+    		$scope.user.push({ 'User age':$scope.user.age, 'activity': $scope.user.activity });
+    		// Writing it to the server
+    		//
+    		var dataObj = {
+    				age : $scope.user.age,
+    				activity : $scope.user.activity,
+    		};
+            var res = $http.post('/user_data_json', dataObj);
+            		res.success(function(data, status, headers, config) {
+            			$scope.message = data;
+            		});
+            res.error(function(data, status, headers, config) {
+            			alert( "failure message: " + JSON.stringify({data: data}));
+            		});
+            		// Making the fields empty
+            		//
+            		$scope.age='';
+            		$scope.activity='';
+    };
 });
