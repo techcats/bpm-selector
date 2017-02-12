b66fc5ee6b4472e12ab32290f544f9dc186aca9d
psychobolt <psychobolt@gmail.com>
2017-02-11 19:26:00 -0800

    Fix issue with serializing User model response

--- src/main/java/com/techcats/bpmselector/controllers/UserController.java
+++ src/main/java/com/techcats/bpmselector/controllers/UserController.java
@@ -4,25 +4,26 @@ import com.techcats.bpmselector.data.models.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.Response;
-
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
     public ResponseEntity<User> createUser(@RequestBody User user)
     {
         return ResponseEntity.ok(new User());
     }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
+    @ResponseBody
     public ResponseEntity<User> updateUser(@RequestBody User user)
     {
         return ResponseEntity.ok(new User());
     }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
     public ResponseEntity<User> getUser(@PathVariable("id")  String id) {
         return ResponseEntity.ok(new User());
     }
--- src/main/java/com/techcats/bpmselector/data/models/User.java
+++ src/main/java/com/techcats/bpmselector/data/models/User.java
@@ -2,4 +2,10 @@ package com.techcats.bpmselector.data.models;
 
 
 public class User {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
 }
